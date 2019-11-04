7e184e235b4772895199c3bfa87a24e4395dfbe0
Your Name <you@example.com>
2019-11-03 19:05:40 -0800

    openpnp and ffmpeg libraries as submodules

---
+++ src/stitcher/main.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+#include <libavcodec/avcodec.h>
+#include <libavformat/avformat.h>
+// #include <ffmpeg/swscale.h>
+
+
+int main(int argc, char *argv[]) {
+
+	printf("Hello\n");
+	return 0;
+}
